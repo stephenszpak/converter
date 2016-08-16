6f000caa208e05f2dcac4fc713d8bb2ef416b9c2
Stephen Szpak <srszpak@gmail.com>
2016-08-15 20:33:59 -0500

    add colors and finished product

--- converter.js
+++ converter.js
@@ -1,22 +1,33 @@
-
 var button = document.getElementById("converter");
 
 button.addEventListener("click", determineConverter);
 
-function toCelsius (input) {
-	return input * 9 / 5 + 32;
+function toCelsius(input) {
+    return input * 9 / 5 + 32;
 };
 
-function toFahrenheit (input) {
-	return (input - 32) * 5 / 9;
+function toFahrenheit(input) {
+    return (input - 32) * 5 / 9;
 };
 
-function determineConverter (clickEvent) {
-	var input = document.getElementById("inputBox").value;
-	if(document.getElementById("c").checked){
-		document.getElementById("converted").innerHTML = "<div>" +toFahrenheit(input) + "</div>"
-	}
-	else if(document.getElementById("f").checked){
-		document.getElementById("converted").innerHTML = "<div>"+ toCelsius(input) + "</div>";
-	}
+function determineConverter(clickEvent) {
+    var input = document.getElementById("inputBox").value;
+    if (document.getElementById("c").checked) {
+        if (input > 90) {
+            document.getElementById("converted").innerHTML = "<span class='red'>" + toFahrenheit(input) + "</span>";
+        } else if (input < 32) {
+            document.getElementById("converted").innerHTML = "<span class='blue'>" + toFahrenheit(input) + "</span>";
+        } else {
+            document.getElementById("converted").innerHTML = "<span>" + toFahrenheit(input) + "</span>";
+        }
+    } else {
+        if (input > 32) {
+            document.getElementById("converted").innerHTML = "<span class='red'>" + toCelsius(input) + "</span>";
+        } else if (input < 0) {
+            document.getElementById("converted").innerHTML = "<span class='blue'>" + toCelsius(input) + "</span>";
+        } else {
+            document.getElementById("converted").innerHTML = "<span>" + toCelsius(input) + "</span>";
+        }
+    }
 };
+
